06f31c9b0dda25800463348e1a45d5680313aef2
AJ HARRIS <THEANTHONYJOHNSON@GMAIL.COM>
2022-05-27 14:18:06 -0500

    wrapped javascript in functions

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-let favAnimal = prompt("What is your favorite animal?");
-console.log(favAnimal);
-document.write("Oh wow! I like " + favAnimal + "s too! Aren't they great?");
+let favAnimal = "";
+let visName = "";
+let favPetname = "";
 
-let visName = prompt("What's your name?");
-console.log(visName);
-document.write(" Nice to mee you " + visName + ", what can I help you with today?");
 
-let favPetname = prompt("What is the name of your favorite pet?");
-console.log(favPetname);
-document.write(favPetname + "? That's a great name!");
+
+
+
+function askAnimal() {
+  favAnimal = prompt("What is your favorite animal?");
+  console.log(favAnimal);
+  document.write("Oh wow! I like " + favAnimal + "s too! Aren't they great?");
+}
+
+function askName() {
+  visName = prompt("What's your name?");
+  console.log(visName);
+  document.write(" Nice to mee you " + visName + ", what can I help you with today?");
+}
+
+function askPet() {
+  favPetname = prompt("What is the name of your favorite pet?");
+  console.log(favPetname);
+  document.write(favPetname + "? That's a great name!");
+}
