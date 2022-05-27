55b4e0c0ea9a96fb0a7025eec43a9fa4b0a6d16f
AJ HARRIS <THEANTHONYJOHNSON@GMAIL.COM>
2022-05-27 13:46:51 -0500

    added javascript functionality

---
+++ app.js
@@ -0,0 +1,7 @@
+let favAnimal = prompt("What is your favorite animal?");
+console.log(favAnimal);
+document.write("Oh wow! I like " + favAnimal + "s too! Aren't they great?");
+
+let visName = prompt("What's your name?");
+console.log(visName);
+document.write(" Nice to mee you " + visName + ", what can I help you with today?");
